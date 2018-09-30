87b4f8c2a6f793decd9b0ba095155aa4c0228b56
Aurelie Bayre <a.bayre@gmail.com>
2018-09-30 17:26:24 +0200

    feat: added statuses

--- src/bot.js
+++ src/bot.js
@@ -2,3 +2,13 @@ const Twit = require('twit')
 const config = require('./config')
 
 const bot = new Twit(config)
+
+bot.post('statuses/update', {
+  status: "Hello World!"
+}, (err, data, response) => {
+  if (err) {
+    console.log(err)
+  }else{
+    console.log(`${data.text} tweeted!`)
+  }
+})
