0936c18f94aad6df6169d1763800a67de80beec9
Aurelie Bayre <a.bayre@gmail.com>
2018-10-08 19:58:23 +0200

    docs: Documenting

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Merci à eux!
 Ce repo est maintenu par:
 * [Aurélie Bayre](https://twitter.com/aurelieBayre)
 
+## Tutos d'Aman Mittal et Scott Spence pour y voir plus clair :
+*A lire dans l'ordre*
+
+1. [Create a Simple Twitter Bot](https://hackernoon.com/create-a-simple-twitter-bot-with-node-js-5b14eb006c08) par Aman Mittal 
+2. [How to Build and Deploy a Multifunctional Twitter Bot](https://medium.freecodecamp.org/how-to-build-and-deploy-a-multifunctional-twitter-bot-49e941bb3092) par Scott Spence 
+
+Je ne peux pas partager les clés d'API, donc je m'occuperais des tests.
+
+Pour les PR: les ouvrir par rapport à la branche develop de ce repo. 
+
--- src/tweetingBot.js
+++ src/tweetingBot.js
@@ -5,7 +5,7 @@ const bot = new Twit(config)
 
 const isReply = require('./helpers/isReply')
 
-
+// un premier test de l'API : récupérer et afficher dans la console les noms des followers
 bot.get(
   'followers/list',
   {
@@ -13,6 +13,8 @@ bot.get(
     count: 200
   },
   (err, data, response) => {
+    // haha je ne sais pas ce que fait cette "response" ici, peut-être un vieux bout de brouillon qui a mal tourné, 
+    // TODO rechercher pour savoir si on peut le virer. A prioiri oui.
     if (err) {
       console.log(err)
     } else {
@@ -22,8 +24,11 @@ bot.get(
     }
   }
 )
-
+// Retweeter les messages contenant #100JoursDeCode
 const retweetRecent = () => {
+  // on lit les tweets récents. 
+  // Ils ne doivent pas contenir les hashtags anglais, seulement le hashtag français. 
+  // (pour ne pas flooder en anglais la communauté francophone.)
   bot.get(
     'search/tweets',
     {
@@ -33,17 +38,21 @@ const retweetRecent = () => {
     (err, data) => {
       if (!err) {
         const recentTweets = data.statuses
+        //on loop sur les récents tweets ainsi obtenus:
         recentTweets.forEach(tweet => {
+          //On ne retweete pas une reply (un retweet ou une réponse à un message contenant #100JoursDeCode)
           if (isReply(tweet)) {
             console.log(`c'est une réponse, on ne retweete pas.`)
             return
           }else {
+            // Autrement, on utilise POST (= pour écrire) pour faire un retweet du message.
             bot.post(
               'statuses/retweet/:id',
               {
                 id: tweet.id_str
               },
               (err, response) => {
+                // j'édite deux messages pour voir ce qu'il se passe dans la console.
                 if (response) {
                   console.log('Retweet réussi :)')
                 }
@@ -62,6 +71,7 @@ const retweetRecent = () => {
   )  
 }
 
+//on appelle cette fonction
 retweetRecent()
-
+// on la répète toutes les 3000000 millisecondes = 50 minutes
 setInterval(retweetRecent, 3000000)
